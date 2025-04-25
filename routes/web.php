4900aa9916e429955c02e6017a2f36e614e19bb5
AntonKorob <gorizont102.k@mail.ru>
2025-04-25 22:13:06 +0700

    add views/about

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello From Hexlet';
+})->name('root');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
